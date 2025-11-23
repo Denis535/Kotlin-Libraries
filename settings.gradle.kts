ec1a6eb37d159922b5ce87b87bcb046683e0bcda
Denis535 <dddenisss35@gmail.com>
2025-11-23 14:06:45 +0200

    root

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,5 @@ rootProject.name = "Kotlin.Libraries"
 include("state-machine-pro")
 include("tree-machine-pro")
 include("game-framework-pro")
+include("game-framework-pro:main")
+include("game-framework-pro:extensions")
