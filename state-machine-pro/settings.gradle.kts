a5945bca019924f8c31c7f9b288a2afa1a12e141
Denis535 <dddenisss35@gmail.com>
2025-11-24 13:39:27 +0200

    state-machine-pro

--- state-machine-pro/settings.gradle.kts
+++ state-machine-pro/settings.gradle.kts
@@ -1,5 +1,19 @@
+pluginManagement {
+    this.repositories {
+        this.gradlePluginPortal()
+        this.mavenCentral()
+    }
+}
+
 plugins {
     this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    this.repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    this.repositories {
+        this.mavenCentral()
+    }
 }
 
 rootProject.name = "state-machine-pro"
