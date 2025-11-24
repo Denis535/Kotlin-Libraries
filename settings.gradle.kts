b2a3fba4d01c2dbde3fb87b06b29d4fbb07be6bc
Denis535 <dddenisss35@gmail.com>
2025-11-24 09:22:36 +0200

    root

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 val isJitpack = System.getenv("JITPACK") == "true"
-val jitpackTag = System.getenv("JITPACK_TAG")
+val version = System.getenv("VERSION")
 
 rootProject.name = "Kotlin-Libraries"
 
-if (!isJitpack || jitpackTag?.startsWith("state-machine-pro/") == true) {
+if (!isJitpack || version?.startsWith("state-machine-pro/") == true) {
     include("state-machine-pro")
 }
-if (!isJitpack || jitpackTag?.startsWith("tree-machine-pro/") == true) {
+if (!isJitpack || version?.startsWith("tree-machine-pro/") == true) {
     include("tree-machine-pro")
 }
-if (!isJitpack || jitpackTag?.startsWith("game-framework-pro/") == true) {
+if (!isJitpack || version?.startsWith("game-framework-pro/") == true) {
     include("game-framework-pro")
 }
-if (!isJitpack || jitpackTag?.startsWith("game-framework-pro-extensions/") == true) {
+if (!isJitpack || version?.startsWith("game-framework-pro-extensions/") == true) {
     include("game-framework-pro-extensions")
 }
