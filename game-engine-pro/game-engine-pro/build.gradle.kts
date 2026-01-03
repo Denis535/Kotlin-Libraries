352fc2765de46182614c115ed6127953502ce966
Denis <dddenisss35@gmail.com>
2026-01-03 19:29:47 +0200

    game-engine-pro

---
+++ game-engine-pro/game-engine-pro/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    this.id("org.jetbrains.kotlin.multiplatform") version "2.3.0"
+}
+
+kotlin {
+    this.mingwX64()
+    this.linuxX64()
+    this.linuxArm64()
+    this.sourceSets {
+        val commonMain by this.getting {
+            this.kotlin.srcDir("sources")
+            this.resources.srcDir("resources")
+            this.dependencies {
+                this.implementation(this.project(":sdl"))
+            }
+        }
+        val mingwX64Main by getting {}
+        val linuxX64Main by getting {}
+        val linuxArm64Main by getting {}
+    }
+}
---
+++ game-engine-pro/sdl/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    this.id("org.jetbrains.kotlin.multiplatform") version "2.3.0"
+}
+
+kotlin {
+    this.mingwX64 {
+        this.compilations["main"].cinterops {
+            val sdl by creating {
+                this.definitionFile = file("sources/com.denis535.sdl-x86_64-w64-mingw32.def")
+            }
+        }
+    }
+    this.linuxX64 {
+        this.compilations["main"].cinterops {
+            val sdl by creating {
+                this.definitionFile = file("sources/com.denis535.sdl-x86_64-linux-gnu.def")
+            }
+        }
+    }
+    this.linuxArm64 {
+        this.compilations["main"].cinterops {
+            val sdl by creating {
+                this.definitionFile = file("sources/com.denis535.sdl-aarch64-linux-gnu.def")
+            }
+        }
+    }
+    this.sourceSets {
+        val commonMain by this.getting {
+            this.kotlin.srcDir("sources")
+            this.resources.srcDir("resources")
+        }
+        val mingwX64Main by getting {}
+        val linuxX64Main by getting {}
+        val linuxArm64Main by getting {}
+    }
+}
