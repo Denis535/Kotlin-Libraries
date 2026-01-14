c807b69aefc65abbef574f5e2e724ce0996e957c
Denis <dddenisss35@gmail.com>
2026-01-14 08:11:21 +0200

    game-engine-pro

---
+++ game-engine-pro/game-engine-pro/sources/com/denis535/game_engine_pro/math/Matrix4x4.kt
@@ -0,0 +1,232 @@
+package com.denis535.game_engine_pro.math
+
+// Column-major
+//    |  0   1   2   3
+// ---+----------------
+// 0  | m00 m10 m20 m30 - x-column: x-direction, translation.x
+// 1  | m01 m11 m21 m31 - y-column: y-direction, translation.y
+// 2  | m02 m12 m22 m32 - z-column: z-direction, translation.z
+// 3  | m03 m13 m23 m33 - w-column: 0, 0, 0, 1
+
+public data class Matrix4x4(
+    public val m00: Float,
+    public val m10: Float,
+    public val m20: Float,
+    public val m30: Float,
+    public val m01: Float,
+    public val m11: Float,
+    public val m21: Float,
+    public val m31: Float,
+    public val m02: Float,
+    public val m12: Float,
+    public val m22: Float,
+    public val m32: Float,
+    public val m03: Float,
+    public val m13: Float,
+    public val m23: Float,
+    public val m33: Float,
+) {
+    public companion object {
+
+        public val Identity: Matrix4x4 = Matrix4x4(
+            1f, 0f, 0f, 0f,
+            0f, 1f, 0f, 0f,
+            0f, 0f, 1f, 0f,
+            0f, 0f, 0f, 1f,
+        );
+
+        public fun TRS(translation: Matrix4x4, rotation: Matrix4x4, scale: Matrix4x4): Matrix4x4 {
+            return translation.Mul(rotation).Mul(scale) // scale-rotation-translation order
+        }
+
+        public fun TRS(position: Vector3, axisX: Vector3, axisY: Vector3, axisZ: Vector3, scale: Vector3): Matrix4x4 {
+            return TRS(Translation(position), Rotation(axisX, axisY, axisZ), Scale(scale))
+        }
+
+        public fun TRS(position: Vector3, angles: Vector3, scale: Vector3): Matrix4x4 {
+            return TRS(Translation(position), Rotation(angles), Scale(scale))
+        }
+
+        public fun Translation(position: Vector3): Matrix4x4 {
+            return Matrix4x4(
+                1f, 0f, 0f, position.X,
+                0f, 1f, 0f, position.Y,
+                0f, 0f, 1f, position.Z,
+                0f, 0f, 0f, 1f,
+            );
+        }
+
+        public fun Rotation(axisX: Vector3, axisY: Vector3, axisZ: Vector3): Matrix4x4 {
+            return Matrix4x4(
+                axisX.X, axisX.Y, axisX.Z, 0f,
+                axisY.X, axisY.Y, axisY.Z, 0f,
+                axisZ.X, axisZ.Y, axisZ.Z, 0f,
+                0f, 0f, 0f, 1f,
+            );
+        }
+
+        public fun Rotation(angles: Vector3): Matrix4x4 {
+            return RotationZ(angles.Z).Mul(RotationY(angles.Y)).Mul(RotationX(angles.X)) // YXZ order
+        }
+
+        public fun RotationX(angleX: Float): Matrix4x4 {
+            val sin = Math.Sin(angleX * Math.DegToRad)
+            val cos = Math.Cos(angleX * Math.DegToRad)
+            return Matrix4x4(
+                1f, 0f, 0f, 0f,
+                0f, cos, sin, 0f,
+                0f, -sin, cos, 0f,
+                0f, 0f, 0f, 1f,
+            )
+        }
+
+        public fun RotationY(angleY: Float): Matrix4x4 {
+            val sin = Math.Sin(angleY * Math.DegToRad)
+            val cos = Math.Cos(angleY * Math.DegToRad)
+            return Matrix4x4(
+                cos, 0f, -sin, 0f,
+                0f, 1f, 0f, 0f,
+                sin, 0f, cos, 0f,
+                0f, 0f, 0f, 1f,
+            )
+        }
+
+        public fun RotationZ(angleZ: Float): Matrix4x4 {
+            val sin = Math.Sin(angleZ * Math.DegToRad)
+            val cos = Math.Cos(angleZ * Math.DegToRad)
+            return Matrix4x4(
+                cos, sin, 0f, 0f,
+                -sin, cos, 0f, 0f,
+                0f, 0f, 1f, 0f,
+                0f, 0f, 0f, 1f,
+            )
+        }
+
+        public fun Scale(scale: Vector3): Matrix4x4 {
+            return Matrix4x4(
+                scale.X, 0f, 0f, 0f,
+                0f, scale.Y, 0f, 0f,
+                0f, 0f, scale.Z, 0f,
+                0f, 0f, 0f, 1f,
+            );
+        }
+
+        public fun Ortho(
+            left: Float,
+            right: Float,
+            bottom: Float,
+            top: Float,
+            zNear: Float,
+            zFar: Float,
+        ): Matrix4x4 {
+            val scaleX = 2f / (right - left)
+            val scaleY = 2f / (top - bottom)
+            val scaleZ = -2f / (zFar - zNear)
+            val offsetX = -(right + left) / (right - left)
+            val offsetY = -(top + bottom) / (top - bottom)
+            val offsetZ = -(zFar + zNear) / (zFar - zNear)
+            return Matrix4x4(
+                scaleX, 0f, 0f, 0f,
+                0f, scaleY, 0f, 0f,
+                0f, 0f, scaleZ, 0f,
+                offsetX, offsetY, offsetZ, 1f,
+            )
+        }
+
+        public fun Frustum(
+            left: Float,
+            right: Float,
+            bottom: Float,
+            top: Float,
+            zNear: Float,
+            zFar: Float,
+        ): Matrix4x4 {
+            val scaleX = 2f * zNear / (right - left)
+            val scaleY = 2f * zNear / (top - bottom)
+            val scaleZ = -(zFar + zNear) / (zFar - zNear)
+            val offsetX = (right + left) / (right - left)
+            val offsetY = (top + bottom) / (top - bottom)
+            val offsetZ = -2f * zFar * zNear / (zFar - zNear)
+            return Matrix4x4(
+                scaleX, 0f, 0f, 0f,
+                0f, scaleY, 0f, 0f,
+                offsetX, offsetY, scaleZ, -1f,
+                0f, 0f, offsetZ, 0f,
+            )
+        }
+
+        public fun Perspective(
+            fov: Float,
+            aspect: Float,
+            zNear: Float,
+            zFar: Float,
+        ): Matrix4x4 {
+            val tanX = Math.Tan(fov * Math.DegToRad / 2) * aspect
+            val tanY = Math.Tan(fov * Math.DegToRad / 2)
+            val left = -tanX * zNear
+            val right = +tanX * zNear
+            val bottom = -tanY * zNear
+            val top = +tanY * zNear
+            return Frustum(left, right, bottom, top, zNear, zFar)
+        }
+
+    }
+
+    public val Position: Vector3
+        get() {
+            return Vector3(m30, m31, m32)
+        }
+
+    public val AxisX: Vector3
+        get() {
+            return Vector3(m00, m10, m20)
+        }
+
+    public val AxisY: Vector3
+        get() {
+            return Vector3(m01, m11, m21)
+        }
+
+    public val AxisZ: Vector3
+        get() {
+            return Vector3(m02, m12, m22)
+        }
+
+    public fun Mul(matrix: Matrix4x4): Matrix4x4 {
+        val m00 = this.m00 * matrix.m00 + this.m01 * matrix.m10 + this.m02 * matrix.m20 + this.m03 * matrix.m30;
+        val m10 = this.m10 * matrix.m00 + this.m11 * matrix.m10 + this.m12 * matrix.m20 + this.m13 * matrix.m30;
+        val m20 = this.m20 * matrix.m00 + this.m21 * matrix.m10 + this.m22 * matrix.m20 + this.m23 * matrix.m30;
+        val m30 = this.m30 * matrix.m00 + this.m31 * matrix.m10 + this.m32 * matrix.m20 + this.m33 * matrix.m30;
+        val m01 = this.m00 * matrix.m01 + this.m01 * matrix.m11 + this.m02 * matrix.m21 + this.m03 * matrix.m31;
+        val m11 = this.m10 * matrix.m01 + this.m11 * matrix.m11 + this.m12 * matrix.m21 + this.m13 * matrix.m31;
+        val m21 = this.m20 * matrix.m01 + this.m21 * matrix.m11 + this.m22 * matrix.m21 + this.m23 * matrix.m31;
+        val m31 = this.m30 * matrix.m01 + this.m31 * matrix.m11 + this.m32 * matrix.m21 + this.m33 * matrix.m31;
+        val m02 = this.m00 * matrix.m02 + this.m01 * matrix.m12 + this.m02 * matrix.m22 + this.m03 * matrix.m32;
+        val m12 = this.m10 * matrix.m02 + this.m11 * matrix.m12 + this.m12 * matrix.m22 + this.m13 * matrix.m32;
+        val m22 = this.m20 * matrix.m02 + this.m21 * matrix.m12 + this.m22 * matrix.m22 + this.m23 * matrix.m32;
+        val m32 = this.m30 * matrix.m02 + this.m31 * matrix.m12 + this.m32 * matrix.m22 + this.m33 * matrix.m32;
+        val m03 = this.m00 * matrix.m03 + this.m01 * matrix.m13 + this.m02 * matrix.m23 + this.m03 * matrix.m33;
+        val m13 = this.m10 * matrix.m03 + this.m11 * matrix.m13 + this.m12 * matrix.m23 + this.m13 * matrix.m33;
+        val m23 = this.m20 * matrix.m03 + this.m21 * matrix.m13 + this.m22 * matrix.m23 + this.m23 * matrix.m33;
+        val m33 = this.m30 * matrix.m03 + this.m31 * matrix.m13 + this.m32 * matrix.m23 + this.m33 * matrix.m33;
+        return Matrix4x4(
+            m00, m10, m20, m30,
+            m01, m11, m21, m31,
+            m02, m12, m22, m32,
+            m03, m13, m23, m33,
+        )
+    }
+
+    public fun Mul(vector: Vector3): Vector3 {
+        val x = this.m00 * vector.X + this.m01 * vector.Y + this.m02 * vector.Z + this.m03;
+        val y = this.m10 * vector.X + this.m11 * vector.Y + this.m12 * vector.Z + this.m13;
+        val z = this.m20 * vector.X + this.m21 * vector.Y + this.m22 * vector.Z + this.m23;
+        val w = this.m30 * vector.X + this.m31 * vector.Y + this.m32 * vector.Z + this.m33;
+        return Vector3(
+            x / w,
+            y / w,
+            z / w,
+        )
+    }
+
+}
---
+++ game-engine-pro/game-engine-pro/sources/com/denis535/game_engine_pro/math/Vector3.kt
@@ -0,0 +1,63 @@
+package com.denis535.game_engine_pro.math
+
+public data class Vector3(
+    public val X: Float,
+    public val Y: Float,
+    public val Z: Float,
+) {
+    public companion object {
+
+        public val Zero: Vector3 = Vector3(0f, 0f, 0f)
+        public val One: Vector3 = Vector3(1f, 1f, 1f)
+
+        public val AxisX: Vector3 = Vector3(1f, 0f, 0f)
+        public val AxisY: Vector3 = Vector3(0f, 1f, 0f)
+        public val AxisZ: Vector3 = Vector3(0f, 0f, 1f)
+
+        public fun Lerp(v0: Vector3, v1: Vector3, t: Float): Vector3 {
+            return Vector3(
+                v0.X + (v1.X - v0.X) * t,
+                v0.Y + (v1.Y - v0.Y) * t,
+                v0.Z + (v1.Z - v0.Z) * t,
+            );
+        }
+
+    }
+
+    public val Length: Float
+        get() {
+            return Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z)
+        }
+
+    public val LengthSquared: Float
+        get() {
+            return this.X * this.X + this.Y * this.Y + this.Z * this.Z
+        }
+
+    public val Normalized: Vector3
+        get() {
+            val length = this.Length;
+            return Vector3(this.X / length, this.Y / length, this.Z / length)
+        }
+
+    public fun Dot(vector: Vector3): Float {
+        return this.X * vector.X + this.Y * vector.Y + this.Z * vector.Z;
+    }
+
+    public fun Cross(vector: Vector3): Vector3 {
+        return Vector3(
+            this.Y * vector.Z - this.Z * vector.Y,
+            this.Z * vector.X - this.X * vector.Z,
+            this.X * vector.Y - this.Y * vector.X,
+        );
+    }
+
+    public operator fun unaryMinus(): Vector3 = Vector3(-this.X, -this.Y, -this.Z)
+
+    public operator fun plus(vector: Vector3): Vector3 = Vector3(this.X + vector.X, this.Y + vector.Y, this.Z + vector.Z)
+    public operator fun minus(vector: Vector3): Vector3 = Vector3(this.X - vector.X, this.Y - vector.Y, this.Z - vector.Z)
+
+    public operator fun times(scalar: Float): Vector3 = Vector3(this.X * scalar, this.Y * scalar, this.Z * scalar)
+    public operator fun div(scalar: Float): Vector3 = Vector3(this.X / scalar, this.Y / scalar, this.Z / scalar)
+
+}
