ce30960bdfceed64a72ba2d8514a250be1d625d7
Denis <dddenisss35@gmail.com>
2026-01-17 10:37:12 +0200

    sdl

--- game-engine-pro/sdl/scripts/install-sdl-aarch64-linux-android.sh
+++
@@ -1,8 +0,0 @@
-sudo docker build \
-    -t install-sdl-aarch64-linux-android \
-    -f docker/install-sdl-aarch64-linux-android.dockerfile .
-
-sudo docker run \
-    --rm \
-    --mount type=bind,source="$PWD/SDL",target=/SDL \
-    install-sdl-aarch64-linux-android
--- game-engine-pro/sdl/scripts/install-sdl-x86_64-linux-gnu.sh
+++
@@ -1,8 +0,0 @@
-sudo docker build \
-    -t install-sdl-x86_64-linux-gnu \
-    -f docker/install-sdl-x86_64-linux-gnu.dockerfile .
-
-sudo docker run \
-    --rm \
-    --mount type=bind,source="$PWD/SDL",target=/SDL \
-    install-sdl-x86_64-linux-gnu
