9352e176b05d8058276e6df65c2a09725e7457ae
Denis <dddenisss35@gmail.com>
2026-01-24 16:53:22 +0200

    game-engine-pro

--- game-engine-pro/game-engine-pro/sources/com/denis535/game_engine_pro/Engine.kt
+++ game-engine-pro/game-engine-pro/sources/com/denis535/game_engine_pro/Engine.kt
@@ -1,6 +1,5 @@
 package com.denis535.game_engine_pro
 
-import com.denis535.game_engine_pro.utils.*
 import com.denis535.sdl.*
 import kotlinx.cinterop.*
 
@@ -67,7 +66,7 @@ public abstract class Engine : AutoCloseable {
                     this@Engine.ProcessEvent(event.ptr)
                 }
             }
-            if (this.Time.Fixed.FrameCount == 0) {
+            if (this.Time.Fixed.FrameCount == 0U) {
                 this.OnFixedUpdate()
                 this.Time.Fixed.DeltaTime = fixedDeltaTime
                 this.Time.Fixed.FrameCount++
@@ -114,6 +113,53 @@ public abstract class Engine : AutoCloseable {
             event.type = SDL_EVENT_QUIT
             SDL_PushEvent(event.ptr).also { SDL.ThrowErrorIfNeeded() }
         }
+    }
+
+}
+
+public class Time {
+
+    public var Time: Float = 0.0f
+        internal set
+
+    public var DeltaTime: Float = 0.0f
+        internal set
+
+    public var FrameCount: UInt = 0U
+        internal set
+
+    public val Fps: Float?
+        get() {
+            return if (this.DeltaTime > 0.0f) 1.0f / this.DeltaTime else null
+        }
+
+    public val Fixed: FixedTime = FixedTime()
+
+    internal constructor()
+
+    public override fun toString(): String {
+        return "Time: ${this.Time}"
+    }
+
+}
+
+public class FixedTime {
+
+    public val Time: Float
+        get() {
+            return this.DeltaTime * this.FrameCount.toFloat()
+        }
+
+    public var DeltaTime: Float = 0.0f
+        internal set
+
+    public var FrameCount: UInt = 0U
+        internal set
+
+    internal constructor()
+
+    public override fun toString(): String {
+        return "FixedTime: ${this.Time}"
     }
 
 }
