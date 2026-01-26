1a8cdd8f333b5ac67bc135f605e98060ca19c666
Denis <dddenisss35@gmail.com>
2026-01-27 00:32:31 +0200

    game-engine-pro

--- game-engine-pro/game-engine-pro/sources/com/denis535/game_engine_pro/utils/Storage.kt
+++ game-engine-pro/game-engine-pro/sources/com/denis535/game_engine_pro/utils/Storage.kt
@@ -21,7 +21,60 @@ public class Storage : AutoCloseable {
     }
 
     @OptIn(ExperimentalForeignApi::class)
-    public fun GetEntityList(path: String): List<String> {
+    public fun CheckEntity(path: String): Boolean {
+        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
+            SDL_Delay(10U).SDL_CheckError()
+        }
+        memScoped {
+            val info = this.alloc<SDL_PathInfo>()
+            return if (SDL_GetStoragePathInfo(this@Storage.NativeStorage, path, info.ptr).SDL_CheckError()) {
+                info.type == SDL_PathType.SDL_PATHTYPE_FILE
+            } else {
+                false
+            }
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    public fun LoadEntity(path: String): ByteArray? {
+        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
+            SDL_Delay(10U).SDL_CheckError()
+        }
+        memScoped {
+            val length = this.alloc<ULongVar>()
+            if (SDL_GetStorageFileSize(this@Storage.NativeStorage, path, length.ptr).SDL_CheckError()) {
+                ByteArray(length.value.toInt()).apply {
+                    this.usePinned {
+                        if (SDL_ReadStorageFile(this@Storage.NativeStorage, path, it.addressOf(0), length.value).SDL_CheckError()) {
+                            return it.get()
+                        }
+                    }
+                }
+            }
+            return null
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    public fun SaveEntity(path: String, data: ByteArray): Boolean {
+        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
+            SDL_Delay(10U).SDL_CheckError()
+        }
+        return data.usePinned {
+            SDL_WriteStorageFile(this@Storage.NativeStorage, path, it.addressOf(0), it.get().size.toULong()).SDL_CheckError()
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    public fun DeleteEntity(path: String): Boolean {
+        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
+            SDL_Delay(10U).SDL_CheckError()
+        }
+        return SDL_RemoveStoragePath(this.NativeStorage, path).SDL_CheckError()
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    public fun GetDirectoryContent(directory: String): List<String>? {
         val callback = staticCFunction<COpaquePointer?, CPointer<ByteVar>?, CPointer<ByteVar>?, SDL_EnumerationResult> { userdata, _, filename ->
             val result = userdata!!.asStableRef<MutableList<String>>().get()
             result += filename!!.toKString()
@@ -30,56 +83,13 @@ public class Storage : AutoCloseable {
         val result = mutableListOf<String>()
         val resultStableRef = StableRef.create(result)
         try {
-            SDL_EnumerateStorageDirectory(this.NativeStorage, path, callback, resultStableRef.asCPointer()).SDL_CheckError()
+            if (SDL_EnumerateStorageDirectory(this.NativeStorage, directory, callback, resultStableRef.asCPointer()).SDL_CheckError()) {
+                return result
+            }
         } finally {
             resultStableRef.dispose()
         }
-        return result
-    }
-
-    @OptIn(ExperimentalForeignApi::class)
-    public fun LoadEntity(path: String): ByteArray? {
-        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
-            SDL_Delay(10U).SDL_CheckError()
-        }
-        val length = memScoped {
-            val length = this.alloc<ULongVar>()
-            if (!SDL_GetStorageFileSize(this@Storage.NativeStorage, path, length.ptr).SDL_CheckError()) {
-                return null
-            }
-            length.value
-        }
-        if (length == 0UL) {
-            return null
-        }
-        if (length > Int.MAX_VALUE.toULong()) {
-            return null
-        }
-        return ByteArray(length.toInt()).apply {
-            this.usePinned {
-                if (!SDL_ReadStorageFile(this@Storage.NativeStorage, path, it.addressOf(0), length).SDL_CheckError()) {
-                    return null
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalForeignApi::class)
-    public fun SaveEntity(path: String, data: ByteArray) {
-        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
-            SDL_Delay(10U).SDL_CheckError()
-        }
-        data.usePinned {
-            SDL_WriteStorageFile(this@Storage.NativeStorage, path, it.addressOf(0), it.get().size.toULong()).SDL_CheckError()
-        }
-    }
-
-    @OptIn(ExperimentalForeignApi::class)
-    public fun DeleteEntity(path: String) {
-        while (!SDL_StorageReady(this.NativeStorage).SDL_CheckError()) {
-            SDL_Delay(10U).SDL_CheckError()
-        }
-        SDL_RemoveStoragePath(this.NativeStorage, path).SDL_CheckError()
+        return null
     }
 
 }
