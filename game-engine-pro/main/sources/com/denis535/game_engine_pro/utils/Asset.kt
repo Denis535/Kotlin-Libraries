939627e01d90de6102c6d06da4e451821c8be5f0
Denis <dddenisss35@gmail.com>
2026-02-01 16:57:59 +0200

    game-engine-pro

--- game-engine-pro/main/sources/com/denis535/game_engine_pro/utils/Asset.kt
+++ game-engine-pro/main/sources/com/denis535/game_engine_pro/utils/Asset.kt
@@ -1,33 +1,33 @@
-package com.denis535.game_engine_pro.utils
-
-public abstract class Asset {}
-
-public abstract class ImageAsset : Asset {
-    public constructor(data: ByteArray) {
-    }
-}
-
-public abstract class TextureAsset : Asset {
-    public constructor(data: ByteArray) {
-    }
-}
-
-public abstract class SoundAsset : Asset {
-    public constructor(data: ByteArray) {
-    }
-}
-
-public abstract class MusicAsset : Asset {
-    public constructor(data: ByteArray) {
-    }
-}
-
-public abstract class FontAsset : Asset {
-    public constructor(data: ByteArray) {
-    }
-}
-
-public abstract class ModelAsset : Asset {
-    public constructor(data: ByteArray) {
-    }
-}
+//package com.denis535.game_engine_pro.utils
+//
+//public abstract class Asset {}
+//
+//public abstract class ImageAsset : Asset {
+//    public constructor(data: ByteArray) {
+//    }
+//}
+//
+//public abstract class TextureAsset : Asset {
+//    public constructor(data: ByteArray) {
+//    }
+//}
+//
+//public abstract class SoundAsset : Asset {
+//    public constructor(data: ByteArray) {
+//    }
+//}
+//
+//public abstract class MusicAsset : Asset {
+//    public constructor(data: ByteArray) {
+//    }
+//}
+//
+//public abstract class FontAsset : Asset {
+//    public constructor(data: ByteArray) {
+//    }
+//}
+//
+//public abstract class ModelAsset : Asset {
+//    public constructor(data: ByteArray) {
+//    }
+//}
