a72b4ea5a11a17382b015f0f0f07e9703e61ad3f
Denis <dddenisss35@gmail.com>
2026-02-08 23:38:51 +0200

    game-engine-pro

--- game-engine-pro/main/build.gradle.kts
+++ game-engine-pro/main/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
             this.kotlin.srcDir("sources")
             this.resources.srcDir("resources")
             this.dependencies {
-                this.api("io.github.denis535:game-engine-pro-utilities:1.0.0")
+                this.api("io.github.denis535:game-engine-pro-utilities:1.0.1")
             }
         }
         val mingwX64Main by getting {}
