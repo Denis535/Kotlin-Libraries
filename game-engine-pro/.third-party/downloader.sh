b624606247c2bcfd0aeac384772d4fbcdb60c79c
Denis <dddenisss35@gmail.com>
2026-02-13 00:19:24 +0200

    .third-party

--- game-engine-pro/.third-party/downloader.sh
+++ game-engine-pro/.third-party/downloader.sh
@@ -2,176 +2,40 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-download_zlib() {
-  DIR="workspace/projects/zlib"
-  REPO="https://github.com/madler/zlib.git"
-  TAG="v1.3.1.2"
+download_repo() {
+  local DIR="$1"
+  local REPO="$2"
+  local TAG="$3"
 
   rm -rf "$DIR"
   mkdir -p "$DIR"
 
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
+  if [ -n "$TAG" ]; then
+    git -c advice.detachedHead=false \
+      clone \
+      --branch "$TAG" \
+      --depth 1 \
+      "$REPO" \
+      "$DIR"
+  else
+    git clone \
+      --depth 1 \
+      "$REPO" \
+      "$DIR"
+  fi
 }
 
-download_png() {
-  DIR="workspace/projects/png"
-  REPO="https://github.com/pnggroup/libpng.git"
-  TAG="v1.6.4"
+download_repo "workspace/projects/zlib"      "https://github.com/madler/zlib.git"               "v1.3.1.2"
+download_repo "workspace/projects/png"       "https://github.com/pnggroup/libpng.git"           "v1.6.4"
+download_repo "workspace/projects/ktx"       "https://github.com/KhronosGroup/KTX-Software.git" "v4.4.2"
+download_repo "workspace/projects/freetype"  "https://github.com/freetype/freetype.git"         "VER-2-14-1"
+download_repo "workspace/projects/ogg"       "https://github.com/xiph/ogg.git"                  "v1.3.6"
+download_repo "workspace/projects/opus"      "https://github.com/xiph/opus.git"                 "v1.6.1"
 
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
+download_repo "workspace/projects/sokol"     "https://github.com/floooh/sokol.git"
+download_repo "workspace/projects/miniaudio" "https://github.com/mackron/miniaudio.git"         "0.11.24"
 
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
-}
-
-download_ktx() {
-  DIR="workspace/projects/ktx"
-  REPO="https://github.com/KhronosGroup/KTX-Software.git"
-  TAG="v4.4.2"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
-}
-
-download_freetype() {
-  DIR="workspace/projects/freetype"
-  REPO="https://github.com/freetype/freetype.git"
-  TAG="VER-2-14-1"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
-}
-
-download_ogg() {
-  DIR="workspace/projects/ogg"
-  REPO="https://github.com/xiph/ogg.git"
-  TAG="v1.3.6"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
-}
-
-download_opus() {
-  DIR="workspace/projects/opus"
-  REPO="https://github.com/xiph/opus.git"
-  TAG="v1.6.1"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
-}
-
-download_sokol() {
-  DIR="workspace/projects/sokol"
-  REPO="https://github.com/floooh/sokol.git"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false clone --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout master
-}
-
-download_miniaudio() {
-  DIR="workspace/projects/miniaudio"
-  REPO="https://github.com/mackron/miniaudio.git"
-  TAG="0.11.24"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false clone --branch "$TAG" --depth 1 "$REPO" "$DIR"
-  git -C "$DIR" -c advice.detachedHead=false checkout "$TAG"
-}
-
-download_sdl() {
-  DIR="workspace/projects/SDL"
-  REPO="https://github.com/libsdl-org/SDL.git"
-  TAG="release-3.4.0"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false \
-    clone \
-    --branch "$TAG" \
-    --depth 1 \
-    "$REPO" \
-    "$DIR"
-}
-
-download_sdl_image() {
-  DIR="workspace/projects/SDL_image"
-  REPO="https://github.com/libsdl-org/SDL_image.git"
-  TAG="release-3.4.0"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false \
-    clone \
-    --branch "$TAG" \
-    --depth 1 \
-    "$REPO" \
-    "$DIR"
-}
-
-download_sdl_ttf() {
-  DIR="workspace/projects/SDL_ttf"
-  REPO="https://github.com/libsdl-org/SDL_ttf.git"
-  TAG="release-3.2.2"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false \
-    clone \
-    --branch "$TAG" \
-    --depth 1 \
-    "$REPO" \
-    "$DIR"
-}
-
-download_sdl_mixer() {
-  DIR="workspace/projects/SDL_mixer"
-  REPO="https://github.com/libsdl-org/SDL_mixer.git"
-  TAG="prerelease-3.1.2"
-
-  rm -rf "$DIR"
-  mkdir -p "$DIR"
-
-  git -c advice.detachedHead=false \
-    clone \
-    --branch "$TAG" \
-    --depth 1 \
-    "$REPO" \
-    "$DIR"
-}
-
-download_zlib
-download_png
-download_ktx
-download_freetype
-download_ogg
-download_opus
-
-download_sokol
-download_miniaudio
-
-download_sdl
-download_sdl_image
-download_sdl_ttf
-download_sdl_mixer
+download_repo "workspace/projects/SDL"       "https://github.com/libsdl-org/SDL.git"            "release-3.4.0"
+download_repo "workspace/projects/SDL_image" "https://github.com/libsdl-org/SDL_image.git"      "release-3.4.0"
+download_repo "workspace/projects/SDL_ttf"   "https://github.com/libsdl-org/SDL_ttf.git"        "release-3.2.2"
+download_repo "workspace/projects/SDL_mixer" "https://github.com/libsdl-org/SDL_mixer.git"      "prerelease-3.1.2"
